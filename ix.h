495992e27a7cc81d3b4aa116a7c732d84502f544
Norrim1 <danilbabenko76840@gmail.com>
2025-05-13 15:44:06 +0700

    Cleaning

--- ix.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-class IX {
-public:
-    virtual void Fx1() = 0;
-    virtual void Fx2() = 0;
-    virtual ~IX() = default;
-};
--- iy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-class IY {
-public:
-    virtual void Fy1() = 0;
-    virtual void Fy2() = 0;
-    virtual ~IY() = default;
-};
